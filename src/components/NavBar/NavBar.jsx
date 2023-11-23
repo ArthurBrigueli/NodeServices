aa6d592be64cddff9c6c05d15bf43625d6408bc6
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-11-23 19:37:11 -0300

    feat: Modificacao navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,30 @@
 import styles from './NavBar.module.css'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { SlArrowDown } from "react-icons/sl";
+
 
 const NavBar = ()=>{
     return(
-        <nav>
+        <nav className={styles.navbar}>
             <div>
 
             </div>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/Produto">Produto</Link>
-                    </li>
-                    <li>
-                        <Link to="/Cliente">Cliente</Link>
-                    </li>
-                    <li>
-                        <Link to="/Pedido">Pedido</Link>
-                    </li>
-                </ul>
-            </div>
+
+            <ul className={styles.ul}>
+                <li>
+                    <Link to="/">Home <SlArrowDown color='white' fontSize={8}/></Link>
+                </li>
+                <li>
+                    <Link to="/Produto">Produto <SlArrowDown color='white' fontSize={8}/></Link>
+                </li>
+                <li>
+                    <Link to="/Cliente">Cliente <SlArrowDown color='white' fontSize={8}/></Link>
+                </li>
+                <li>
+                    <Link to="/Pedido">Pedido <SlArrowDown color='white' fontSize={8}/></Link>
+                </li>
+            </ul>
+
             <div>
                 
             </div>
